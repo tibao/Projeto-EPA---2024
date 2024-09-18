73e7537fbefad8d81d9e20863016d7af50db0629
Vinicius Tibo <viniciustiboff@gmail.com>
2024-09-18 17:10:18 -0300

    desculpa acabei fazendo o commit em sua conta por engano

--- games/dino.php
+++
@@ -1,4 +0,0 @@
-<script src="../script/dino.js"></script>
-<link rel="stylesheet" href="../style/dino.css">
-
-<canvas id="board"></canvas>
--- script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('keydown', function (event) {
-    if (event.key === 'ArrowDown' || event.key === 'ArrowUp' || event.key === ' ') {
-        event.preventDefault();
-    }
-});
